5df59c1ff7fbb5d4b31660dc1841ba127cfcf94f
Francis Akpan <francisbnsn14@gmail.com>
2020-12-27 15:53:52 +0100

    Update came_case solution

--- kotlin/src/main/kotlin/came_case/camel-case.kt
+++ kotlin/src/main/kotlin/came_case/camel-case.kt
@@ -4,11 +4,9 @@ fun main() {
     println(camelcase(""))
 }
 
-fun camelcase(s: String): Int {
+fun camelcase(s: String): Int = s.split().map {
+    if (it.single().isUpperCase()) 1
+    else 0
+}.fold(1, { prev, element -> prev + element })
 
-    return s.split("").map {
-        if (it.trim() != "" && it.single().isUpperCase()) 1
-        else 0
-    }.fold(1, {prev, element -> prev + element} )
-
-}
+fun String.split(): Array<String> = Array(this.length) { this[it].toString() }
